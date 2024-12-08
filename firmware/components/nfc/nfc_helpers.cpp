4188c32b18f2b2cdc47c3f3c9637b6324397fd5c
Spencer Owen <owenspencer@gmail.com>
2024-12-07 19:09:16 -0700

    Read all tags

--- firmware/components/nfc/nfc_helpers.cpp
+++ firmware/components/nfc/nfc_helpers.cpp
@@ -47,17 +47,18 @@ std::string get_random_ha_tag_ndef() {
 }
 
 
-std::vector<uint8_t> hmac_sha256(const std::vector<uint8_t>& key, const std::vector<uint8_t>& data) {
-    std::vector<uint8_t> output(32);
-    mbedtls_md_context_t ctx;
-    mbedtls_md_init(&ctx);
-    mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(MBEDTLS_MD_SHA256), 1);
-    mbedtls_md_hmac_starts(&ctx, key.data(), key.size());
-    mbedtls_md_hmac_update(&ctx, data.data(), data.size());
-    mbedtls_md_hmac_finish(&ctx, output.data());
-    mbedtls_md_free(&ctx);
-    return output;
-}
+//TODO: remove
+// std::vector<uint8_t> hmac_sha256(const std::vector<uint8_t>& key, const std::vector<uint8_t>& data) {
+//     std::vector<uint8_t> output(32);
+//     mbedtls_md_context_t ctx;
+//     mbedtls_md_init(&ctx);
+//     mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(MBEDTLS_MD_SHA256), 1);
+//     mbedtls_md_hmac_starts(&ctx, key.data(), key.size());
+//     mbedtls_md_hmac_update(&ctx, data.data(), data.size());
+//     mbedtls_md_hmac_finish(&ctx, output.data());
+//     mbedtls_md_free(&ctx);
+//     return output;
+// }
 
 // std::array<const uint8_t*, 16> generate_keys() {
 //     std::vector<uint8_t> uid = {0x5A, 0xC9, 0x00, 0xA6};
@@ -92,8 +93,8 @@ std::vector<uint8_t> hmac_sha256(const std::vector<uint8_t>& key, const std::vec
 
 //TODO: should generate_keys() use pointers? 
 //TODO: should generate_keys() use a vector? 
-std::vector<std::vector<uint8_t>> generate_keys() {
-    std::vector<uint8_t> uid = {0x5A, 0xC9, 0x00, 0xA6};
+std::vector<std::vector<uint8_t>> generate_keys(std::vector<uint8_t>& uid) {
+    // std::vector<uint8_t> uid = {0x5A, 0xC9, 0x00, 0xA6};
     std::vector<uint8_t> master = {
         0x9a, 0x75, 0x9c, 0xf2, 0xc4, 0xf7, 0xca, 0xff,
         0x22, 0x2c, 0xb9, 0x76, 0x9b, 0x41, 0xbc, 0x96
--- firmware/components/nfc/nfc_helpers.h
+++ firmware/components/nfc/nfc_helpers.h
@@ -14,7 +14,7 @@ std::string get_random_ha_tag_ndef();
 bool has_ha_tag_ndef(NfcTag &tag);
 // std::vector<int> generate_keys();
 // std::array<const uint8_t*, 16> generate_keys();
-std::vector<std::vector<uint8_t>> generate_keys();
+std::vector<std::vector<uint8_t>> generate_keys(std::vector<uint8_t> &uid);
 
 }  // namespace nfc
 }  // namespace esphome
